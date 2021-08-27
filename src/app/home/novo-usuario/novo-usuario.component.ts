8494910dd3837459100320743fdf45f2198be058
EvandroOliveira495 <evandro.ricardo@hotmail.com>
2021-08-26 22:01:22 -0300

    validacao de senha adicionado

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { minusculoValidator } from './minusculo.validator';
+import { NovoUsuario } from './novo-usuario';
+import { NovoUsuarioService } from './novo-usuario.service';
+import { UsuarioExisteService } from './usuario-existe.service';
+import { usuarioSenhaIguaisValidator } from './usuario-senha-iguais.validator';
 
 @Component({
   selector: 'app-novo-usuario',
@@ -6,10 +12,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./novo-usuario.component.css']
 })
 export class NovoUsuarioComponent implements OnInit {
+  novoUsuarioForm!: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private novoUsuarioService: NovoUsuarioService,
+    private usuarioExistenteService: UsuarioExisteService
+  ) { }
 
   ngOnInit(): void {
+    this.novoUsuarioForm = this.formBuilder.group(
+      {
+        email: ['', [Validators.required, Validators.email]],
+        fullName: ['', [Validators.required, Validators.minLength(4)]],
+        userName: ['', [minusculoValidator]],
+        password: [''],
+      },{
+        validators:[usuarioSenhaIguaisValidator],
+      }
+      );
+  }
+
+  cadastrar() {
+    const novoUsario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
+    console.log(novoUsario);
   }
 
 }
